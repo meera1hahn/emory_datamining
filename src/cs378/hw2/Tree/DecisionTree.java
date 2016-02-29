67501f5f6581f0453ea7456cdbdd06f2104be362
Shureed <skabir1717@gmail.com>
2016-02-29 16:27:49 -0500

    basic skeleton

---
+++ src/cs378/hw2/Tree/DecisionTree.java
@@ -0,0 +1,27 @@
+package cs378.hw2.Tree;
+
+import cs378.hw2.AbstractClassifier;
+
+import java.util.List;
+
+/**
+ * Created by shureedkabir on 2/29/16.
+ */
+public class DecisionTree extends AbstractClassifier {
+
+
+    public DecisionTree(){
+
+    }
+
+    @Override
+    public String classify(String[] features) {
+        return null;
+    }
+
+    @Override
+    public void train(List<String[]> trainingData) {
+
+    }
+
+}
---
+++ src/cs378/hw2/Tree/DecisionTreeNode.java
@@ -0,0 +1,14 @@
+package cs378.hw2.Tree;
+
+/**
+ * Created by shureedkabir on 2/29/16.
+ */
+public class DecisionTreeNode {
+    DecisionTreeNode left, right, parent;
+
+    public DecisionTreeNode(){
+
+    }
+
+
+}
